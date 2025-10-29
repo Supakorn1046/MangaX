918af96833664d70f2c5a7ce376bc0f2d7e3a813
TunyawutShin <tunyawut.m@ku.th>
2025-10-29 23:15:33 +0700

    add_addminlist,add_adminadd,addicon

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,12 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import Homepage from './Web/Homepage.jsx'
+import ProductPage from './Web/admin_list.jsx';
+import AdminAdd from'./Web/admin_add.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Homepage />
+    <AdminAdd />
+    <ProductPage/>
   </StrictMode>,
 )
