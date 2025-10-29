bca3472ea6db25ff69f027594340a1c2769c73df
TunyawutShin <tunyawut.m@ku.th>
2025-10-30 00:42:00 +0700

    addProductdetail

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,10 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import Homepage from './Web/Homepage.jsx'
+import ProductDetailPage from './Web/ProductDetailPage.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Homepage />
+    <ProductDetailPage />
   </StrictMode>,
 )
